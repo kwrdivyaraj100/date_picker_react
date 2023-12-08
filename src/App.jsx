3339923b259d2debe9d9d8b95fb001a0c4f57ecd
Divya R <kwrdivyaraj100@gmail.com>
2023-12-08 12:57:03 +0530

    Added state for date range and refOne useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Chip, Divider } from '@mui/material';
 import CalenderComp from './components/CalenderComp';
 import DateRangeComp from './components/DateRangeComp';
+import DateRangePickerComp from './components/DateRangePickerComp';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
 
       <Divider><Chip label="Date Range" /></Divider>
       <DateRangeComp />
+
+      <Divider><Chip label="Date Range" /></Divider>
+      <DateRangePickerComp />
     </>
   );
 }
